2b937e84e42ed09e155e2163c60ce625a2e57869
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-01-10 16:39:09 -0500

    fix cli inputs

--- erddap_checks/__main__.py
+++ erddap_checks/__main__.py
@@ -13,19 +13,20 @@ from erddap_checks.erddap import Erddap
     "datasets_xml",
     envvar="ERDDAP_DATASETS_XML",
     type=str,
-    nargs=-1,
+    nargs=1,
 )
 @click.option("-k", "--test-filter", help="Run tests by keyword expressions", type=str)
 def main(datasets_xml, test_filter):
     """Run a series of tests on ERDDAP datasets"""
-    if not datasets_xml and glob("**/datasets.xml", recursive=True):
-        logger.info("Load **/datasets.xml")
-        datasets_xml = "**/datasets.xml"
-    elif not datasets_xml and glob("**/datasets.d/*.xml", recursive=True):
-        logger.info("Load **/datasets.d/*.xml folder")
-        datasets_xml = "**/datasets.d/*.xml"
-    else:
-        raise ValueError("No datasets.xml found")
+    if not datasets_xml:
+        if glob("**/datasets.xml", recursive=True):
+            logger.info("Load **/datasets.xml")
+            datasets_xml = "**/datasets.xml"
+        elif glob("**/datasets.d/*.xml", recursive=True):
+            logger.info("Load **/datasets.d/*.xml folder")
+            datasets_xml = "**/datasets.d/*.xml"
+        else:
+            raise ValueError("No datasets.xml found")
 
     os.environ["ERDDAP_DATASETS_XML"] = datasets_xml
     args = []
